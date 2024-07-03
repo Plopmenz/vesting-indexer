7806748b2029887526fc31efd38e16efa12004e0
Plopmenz <plopmenz@gmail.com>
2024-07-03 10:35:32 +0200

    filter returns events directly

--- api/return-types.ts
+++ api/return-types.ts
@@ -1,6 +1,3 @@
-import { EventIdentifier } from "../types/event-identifier.js";
 import { Event } from "../types/storage.js";
 
-export type EventReturn = Event;
-
-export type FilterEventsReturn = EventIdentifier[];
+export type FilterEventsReturn = Event[];
